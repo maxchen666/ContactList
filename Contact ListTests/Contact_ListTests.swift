0130d74e80f5c27223963fc9d2e2b7e25dba3d17
Yong Chen <maxchen666@gmail.com>
2018-05-06 17:28:27 +1000

    Add the unit test for service

--- Contact ListTests/Contact_ListTests.swift
+++ Contact ListTests/Contact_ListTests.swift
@@ -10,20 +10,29 @@ import XCTest
 @testable import Contact_List
 
 class Contact_ListTests: XCTestCase {
+    var contactService: ContactService!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        contactService = ContactService()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        contactService = nil
     }
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        contactService.LoadContacts(){
+            contacts in
+            
+            XCTAssertFalse(contacts.count > 0)
+        }
     }
     
     func testPerformanceExample() {
